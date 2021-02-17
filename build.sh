1a2a115ebe7b44a822cb7a5cdd1bb8e0f47f2cc2
tinnudan <tinnuadan@gmail.com>
2021-02-17 20:45:33 +0100

    deleting old files

--- build.sh
+++ build.sh
@@ -9,5 +9,18 @@ cd PaperBuild
 bash build.sh
 
 cd $rootdir
+#copy new files
 cp PaperBuild/build . -ur
+#remove temporary build files
 rm -rv PaperBuild/build
+
+#remove all but n builds
+n=10
+cd build
+for branch in *; do
+    if $(test -d $branch); then
+        cd $branch
+        ls -tp | grep -v '/$' | tail -n +$(expr $n + 1) | xargs -I {} rm -- {}    
+        cd ..
+    fi
+done
