7b548bd960fca553c7ed30805338d7e7294d02f2
tinnuadan <tinnuadan@gmail.com>
2021-02-17 11:35:27 +0100

    copy build files to output directory

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+rootdir=/app/
+
 git clone https://github.com/tinnuadan/PaperBuild.git
 cp config.ini PaperBuild/
 cd PaperBuild
 bash build.sh
+
+cd $rootdir
+cp PaperBuild/build . -ur
+rm -rv PaperBuild/build
